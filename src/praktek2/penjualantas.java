83beebbcdbb1de30c318ab0936ba4240bfece553
Habib06 <habib06@gmail.com>
2018-04-01 13:01:03 +0800

    Membuat Atribut dan Behavior

--- src/praktek2/penjualantas.java
+++ src/praktek2/penjualantas.java
@@ -1,6 +1,14 @@
 package praktek2;
-
 public class penjualantas {
-     
+    int harga_tas;
+    int jumlah_barang;
     
+    
+ void cetakInfo(){
+    System.out.println("================================");
+    System.out.println("harga_tas     :"+harga_tas);
+    System.out.println("jumlah_barang     :"+jumlah_barang);
+    System.out.println("================================");
+}
+
 }
