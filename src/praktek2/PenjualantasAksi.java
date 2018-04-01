88bdb6afae7e60d4ae3e0cdf83c5bf44eaf80ea9
Habib06 <habib06@gmail.com>
2018-04-01 13:09:29 +0800

    Instance Object

--- src/praktek2/PenjualantasAksi.java
+++ src/praktek2/PenjualantasAksi.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek2;
 
-/**
- *
- * @author Asus
- */
 public class PenjualantasAksi {
-    
+     public static void main(String[] args) {
+        penjualantas r1 = new penjualantas();
+        
+        
+        
+    }
 }
+
+
+
+
+
+
