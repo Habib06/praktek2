5e8a9ddd06779b24daf96ac1d9f855a06474a482
Habib06 <habib06@gmail.com>
2018-04-01 13:11:22 +0800

    Mengisi nilai atribute

--- src/praktek2/PenjualantasAksi.java
+++ src/praktek2/PenjualantasAksi.java
@@ -3,9 +3,10 @@ package praktek2;
 public class PenjualantasAksi {
      public static void main(String[] args) {
         penjualantas r1 = new penjualantas();
-        
-        
-        
+
+        r1.harga_tas = 1000000;
+        r1.jumlah_barang = 5;        
+            
     }
 }
 
